58b452c93a987f075b3131b21dea992e2662daea
Orifhon <hitadorg@gmail.com>
2017-01-17 09:23:55 -0500

    Initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.services';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import {AppRoutingModule} from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service'; 
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpModule, InMemoryWebApiModule.forRoot(InMemoryDataService)],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/dashboard.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { HeroService } from './hero.services';
+import { Hero } from './hero';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-dashboard',
+    templateUrl: 'dashboard.component.html',
+    styleUrls: ['dashboard.component.css']
+})
+export class DashboardComponent implements OnInit{
+    constructor(private heroService:HeroService){}
+    heroes:Hero[] = [];
+    ngOnInit():void{
+        this.heroService.getHeroes().then(heroes=>this.heroes=heroes.slice(1,5));
+    }
+}
